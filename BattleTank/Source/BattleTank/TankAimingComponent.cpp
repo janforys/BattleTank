aa2b8dacd01fe04a4d1d2cc16595699f441b4865
janforys <janforys@wp.pl>
2018-04-18 12:27:36 +0200

    Creating Projectile class, Projectile blueprint and adding firing method

--- BattleTank/Source/BattleTank/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/TankAimingComponent.cpp
@@ -72,16 +72,8 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
+		
 	}
-
-	else
-	{
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: NO aim solution found"), Time);
-	}
-
 }
 
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)

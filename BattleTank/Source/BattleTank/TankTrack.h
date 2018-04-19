05b3304dff087254ea979e288c6a51981e504a68
janforys <janforys@wp.pl>
2018-04-19 22:29:15 +0200

    Force & Friction

--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -19,5 +19,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
+	UPROPERTY(EditDefaultsOnly, Category = TankTrack)
+		float TrackMaxDrivingForce = 400000;
 	
 };
